9658de1c6f0c0a8e311059b6694d0986bddf1068
dannyngapptisan <55778646+dannyngapptisan@users.noreply.github.com>
2019-11-08 18:26:05 +0800

    增加了读取artwork的方法
    用此链接读取http://localhost:3000/artworks

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const fs = require('fs');
+
+rawdata = fs.readFileSync('data.json');
+data = JSON.parse(rawdata);
+
+app.get('/artworks', (req, res) => res.send(data.artworks))
+
+
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
