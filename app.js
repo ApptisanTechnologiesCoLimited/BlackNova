fe0388cadb981f94488e16aea2dec2f79c1e1780
Danny Ng <danny.ng@apptisan.com>
2019-11-09 14:46:33 +0800

    增删改查5个方法已全部添加

--- app.js
+++ app.js
@@ -5,7 +5,25 @@ const fs = require('fs');
 rawdata = fs.readFileSync('data.json');
 data = JSON.parse(rawdata);
 
-app.get('/artworks', (req, res) => res.send(data.artworks))
+app.get('/artworks', function (req, res) {
+  res.send(data.artworks)
+})//get all
+
+app.get('/artworks/:id',function (req, res) {
+  res.send(data.artworks)
+})//get one
+
+app.post('/artworks',function (req, res) {
+  res.send(1)// 1 = success, 0 = fail
+})//add one
+
+app.put('/artworks/:id', function (req, res) {
+  res.send(1)
+})//update one
+
+app.delete('/artworks/:id',function (req, res) {
+  res.send(1)
+})//delete one
 
 
 
