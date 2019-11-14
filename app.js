43aaa33b4b3f2abf81c51a51e636a15e28646468
Danny NG <danny.ng198811@Gmail.com>
2019-11-14 11:18:33 +0800

    前端以AJAX方式（PUT方法）访问/artwork/{ID}时，服务器应更新该ID的数据，并放回结果

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const mustacheExpress = require('mustache-express');
 var format = require('date-format');
-
+var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
 
@@ -21,8 +21,8 @@ app.use(express.static('public'))
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
-
-
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 
 rawdata = fs.readFileSync('data.json');
@@ -42,6 +42,19 @@ app.get('/artworks', function (req, res) {
   });
 
 })//get all..
+app.get('/artworks/:page_id', function (req, res) {
+
+  connection.query('SELECT * from artwork', function (error, results, fields) {
+    if (error) throw error;
+    var artworks =[];
+    for (i = 0; i < results.length; i++) {
+      results[i].changedate = format("dd.MM.yyyy",results[i].changedate);
+      artworks.push(results[i]);
+    }
+    res.render('artworks',{"artworks":artworks});
+  });
+
+})//get by pages..
 app.get('/artwork/:id',function (req, res) {
   connection.query('SELECT * from artwork where id ='+req.params.id, function (error, results, fields) {
     if (error) throw error;
@@ -66,8 +79,11 @@ app.post('/artwork/',function (req, res) {
 
 
 })//update an artwork
-app.put('/artwork/',function (req, res) {
-  connection.query('insert into artwork(name,product,protocol,changedate,img) values( CONCAT("Artwork ",CEILING(RAND()*900+100)),"a product","cccc","2019-12-1","/images/pic1.jpg");', function (error, results, fields) {
+app.put('/artwork/:id',function (req, res) {
+
+  var str = "update artwork set name = '"+req.body.name+"', protocol = '"+req.body.protocol+"' where id = "+req.params.id;
+
+  connection.query(str, function (error, results, fields) {
     if (error) {throw error;}
     res.send("success");
   });
