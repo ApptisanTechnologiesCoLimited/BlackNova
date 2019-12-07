3f15a0478325ef5d1cd08d56657abcb11af19ff8
dannyngapptisan <55778646+dannyngapptisan@users.noreply.github.com>
2019-12-07 13:50:54 +0800

    added step2

--- routes/step2.js
+++ routes/step2.js
@@ -11,9 +11,27 @@ router.get('/frames/', function (req, res) {
 
 })//get all..
 
+router.get('/frame/:fid', function (req, res) {
+
+    connection.query('SELECT * from frame where id = '+req.params.fid, function (error, results, fields) {
+        if (error) throw error;
+        res.send(results);
+    });
+
+})//get all..
+
 router.get('/buttons/:fid', function (req, res) {
 
     connection.query('SELECT * from button where fid='+req.params.fid, function (error, results, fields) {
+        if (error) throw error;
+        res.send(results);
+    });
+
+})//get all..
+
+router.get('/button/:id', function (req, res) {
+
+    connection.query('SELECT * from button where id='+req.params.id, function (error, results, fields) {
         if (error) throw error;
         res.send(results);
     });
